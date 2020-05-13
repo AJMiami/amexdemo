da5926d73ce02ea6d0ab1c970ffd9c16eef7b86e
JThott <jthott8@gmail.com>
2020-05-13 18:03:42 -0400

    Cleaning up

--- src/test/java/com/amex/accountservice/web/AccountInfoControllerTest.java
+++ src/test/java/com/amex/accountservice/web/AccountInfoControllerTest.java
@@ -1,6 +1,5 @@
 package com.amex.accountservice.web;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -10,15 +9,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -27,10 +23,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.amex.accountservice.service.AccountInfoService;
 import com.amex.accountservice.utils.TestUtils;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 @AutoConfigureMockMvc
-
 public class AccountInfoControllerTest {
 
     @Mock
