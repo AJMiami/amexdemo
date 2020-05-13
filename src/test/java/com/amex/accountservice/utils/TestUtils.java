40852117661280f976d02f3f735a5d1a131187aa
AJMiami <65269296+AJMiami@users.noreply.github.com>
2020-05-13 17:48:36 -0400

    MIssed to push the code along with last commit. Made changes to support latest changes in Webtest

--- src/test/java/com/amex/accountservice/utils/TestUtils.java
+++ src/test/java/com/amex/accountservice/utils/TestUtils.java
@@ -2,6 +2,7 @@ package com.amex.accountservice.utils;
 
 import com.amex.accountservice.vo.Account;
 import com.amex.accountservice.vo.Address;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * the class {@link TestUtils} contains helper methids t full fill testing
@@ -17,10 +18,24 @@ public class TestUtils {
     
     public static Account getAccountInfo() {
         Account account=new Account();
-        account.setAddress(new Address());
+        account.setAddress(new Address("address1","Address2","city", "state", "12345"));
         account.setTelephoneNumber(1234567890L);
         return account;
         
+    }
+    
+    /**
+     * method to converto an object to JSON String
+     * @param obj
+     * @return
+     */
+    
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
