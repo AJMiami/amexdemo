994d5a08dceb0a1acbe5cccb7fd7f6bab7fd911f
AJMiami <65269296+AJMiami@users.noreply.github.com>
2020-05-13 17:55:22 -0400

    Changed return statuses
    This is part of the last commit

--- src/main/java/com/amex/accountservice/web/AccountInfoController.java
+++ src/main/java/com/amex/accountservice/web/AccountInfoController.java
@@ -2,9 +2,13 @@ package com.amex.accountservice.web;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import javax.xml.ws.spi.http.HttpHandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,34 +39,37 @@ public class AccountInfoController {
      */
     @ResponseBody
     @GetMapping(value="/{accountNumber}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Optional<Account> get(@PathVariable Long accountNumber){
+    public ResponseEntity<Optional<Account>> get(@PathVariable Long accountNumber){
         
-               
-        return Optional.of(accountService.get(accountNumber));
+        HttpHeaders responseHeaders= new HttpHeaders();               
+        return new ResponseEntity<Optional<Account>>(accountService.get(accountNumber),responseHeaders,HttpStatus.OK);
         
         // TODO add AOP to send back HTTP status 204 if no content is returned.
     }
-    
-    @GetMapping(value="/",  produces = MediaType.APPLICATION_JSON_VALUE)
-    public String get(){
-        accountService.getAll(); 
-        return "its me";
-    }
-    
+      
     /**
      * api exposed to persist account information
      * @param accountInfo
+     * @return 
      * @return
      */
     @ResponseBody
     @PostMapping(value="/" , consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void save(@RequestBody Account account){
+    public ResponseEntity<Object> save(@RequestBody Account account){
+        
+        HttpHeaders responseHeaders= new HttpHeaders();               
+        ResponseEntity<Object>obj= new ResponseEntity<>(null,responseHeaders,HttpStatus.CREATED);
+        
         try {
             accountService.save(account);
+            
+            
         }catch(Exception e) {
             //cusotm exceptions should be added, based on the company rule, bubble it and throw it back to the client or catch and report it at this API level
             System.out.println(e);
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        return obj;
         
         
     }
